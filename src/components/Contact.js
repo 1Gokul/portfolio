6d5a220f293646379fbb004124baebe78cfb850f
1Gokul <viswanath1gokul@gmail.com>
2021-08-05 21:57:40 +0530

    Fixed reddit link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ const Contact = () => {
           <SocialIconLink icon={VscGithub} link="https://github.com/1Gokul"/>
           <SocialIconLink icon={IoLogoLinkedin} link="https://www.linkedin.com/in/gokul-viswanath-31383b193/"/>
           <SocialIconLink icon={VscTwitter} link="https://twitter.com/1GokulV"/>
-          <SocialIconLink icon={RiRedditFill} link="https://twitter.com/1GokulV"/>
+          <SocialIconLink icon={RiRedditFill} link="https://old.reddit.com/user/_zygoat/"/>
         </Flex>
       </Box>
 
