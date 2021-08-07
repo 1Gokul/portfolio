fa7300a81982b5701fa8fd6922c2530e7dd5e77f
1Gokul <viswanath1gokul@gmail.com>
2021-08-07 19:12:50 +0530

    Slightly changed title heading

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,6 +11,8 @@ const Intro = () => {
           <Heading
             marginBottom={5}
             fontSize={{ base:"6xl", md: "7xl" }}
+            fontWeight="extrabold"
+            letterSpacing="-3.5px"
             bgGradient="linear(to-r, #8000FF, #30FFB3, #07F49E)"
             bgClip="text">Gokul Viswanath.</Heading>
           <Heading size="xl"> I am a full-stack web developer.<br/>I also make games on the side.</Heading>
