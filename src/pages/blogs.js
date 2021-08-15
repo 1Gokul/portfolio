22a1076dcab40a87324dbc6c6fe4df7aa3b8ebf7
1Gokul <viswanath1gokul@gmail.com>
2021-08-15 23:51:49 +0530

    attempt to fix blog url creation 2

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,23 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
+
 import Layout, {
   BlogContainer,
   SectionHeading,
 } from "../components/Layout/Layout"
-import {
-  LinkBox,
-  LinkOverlay,
-  Heading,
-  Text,
-  Flex,
-} from "@chakra-ui/layout"
+import { LinkBox, LinkOverlay, Heading, Text, Flex } from "@chakra-ui/layout"
 import { Link as GatsbyLink } from "gatsby"
 import Seo from "../components/SEO"
 
 const Blogs = ({ data }) => {
-  const  posts  = data.allMdx.nodes
-
-  console.log(posts)
+  const { edges: posts } = data.allMdx
 
   return (
     <Layout type="blog">
@@ -27,16 +20,16 @@ const Blogs = ({ data }) => {
         <SectionHeading>My Blogs</SectionHeading>
 
         <Flex flexDirection="column" justifyContent="space-evenly">
-          {posts.map (post => (
+          {posts.map (({ node: post }) => (
             <LinkBox
-              key={post.frontmatter.title}
+              key={post.id}
               marginY={5}
               transition="0.2s linear"
               _hover={{
                 color: "aqua.300",
               }}
             >
-              <LinkOverlay as={GatsbyLink} href={`blog/${post.slug}`}>
+              <LinkOverlay as={GatsbyLink} href={post.fields.slug}>
                 <Heading>{post.frontmatter.title}</Heading>
               </LinkOverlay>
               <Text fontSize="sm">{post.frontmatter.date}</Text>
@@ -55,13 +48,16 @@ export default Blogs
 export const pageQuery = graphql`
 {
   allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-    nodes {
-      id
-      slug
-      excerpt
-      frontmatter {
-        date(formatString: "DD MMMM, YYYY")
-        title
+    edges {
+      node {
+        id
+        excerpt
+        frontmatter {
+          title
+        }
+        fields {
+          slug
+        }
       }
     }
   }
