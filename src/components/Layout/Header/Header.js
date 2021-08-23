c061d49dbfaeda58e90c2cde8020d525ee9a8ca2
1Gokul <viswanath1gokul@gmail.com>
2021-08-23 19:13:18 +0530

    removed unused imports

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { Flex, IconButton } from "@chakra-ui/react"
+import React from "react"
+import { Flex } from "@chakra-ui/react"
 
 import Logo from "./Logo"
 import MobileNavMenu from "./MobileNavMenu"
